a3ad0c9de743d5704f7a857368df122de6e2862d
Ivaylo Traykov <ivaylotraykov027@gmail.com>
2021-04-05 23:47:39 +0300

    another miserable attempt at unit testing

--- src/test/java/bg/softuni/pethotel/web/CommentControllerTest.java
+++ src/test/java/bg/softuni/pethotel/web/CommentControllerTest.java
@@ -6,22 +6,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
-import javax.transaction.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@AutoConfigureTestDatabase
-@Transactional
 public class CommentControllerTest {
-
 
     @Autowired
     private MockMvc mockMvc;
@@ -55,7 +49,7 @@ public class CommentControllerTest {
 
     @Test
     public void testCommentsGetReturnsCorrectStatus() throws Exception {
-        this.mockMvc.perform(get("/comments/pages?=0"))
+        this.mockMvc.perform(get("/comments/pages?page=0"))
                 .andExpect(status().isOk());
     }
 }
